42bc42faf64bde9547349bcfeb132e99b5dfecb8
Arif Islam <arif@dreamfactory.com>
2016-06-25 19:17:34 -0400

    update

--- run.php
+++ run.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-$_int = 2; //2 seconds
+$_int = 3; //3 seconds
 $watcher = new \a15lam\WemoPlex\Watcher();
 \a15lam\WemoPlex\Logger::$silent = false;
 
