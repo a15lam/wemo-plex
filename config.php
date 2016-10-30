1ff350a709af34e82c1f0942f1c03080dcd829cd
Arif Islam <arif@dreamfactory.com>
2016-10-30 15:00:32 -0400

    updating config

--- config.php
+++ config.php
@@ -20,7 +20,7 @@ return [
     /*
      * Set log level. Honors hierarchy of ERROR->WARNING->INFO->DEBUG
      */
-    'log_level'      => \a15lam\Workspace\Utility\Logger::DEBUG,
+    'log_level'      => \a15lam\Workspace\Utility\Logger::ERROR,
     /*
      * Set the directory where log file will be written. 
      */
